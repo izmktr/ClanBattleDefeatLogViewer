de5451304576b5076960ee56b79ccf0197bc950a
izmktr <izmktr@gmail.com>
2020-09-27 06:50:04 +0900

    凸推移、凸速度のグラフ表示を追加

--- CranBattleDefeatLogViewer/Form1.Designer.cs
+++ CranBattleDefeatLogViewer/Form1.Designer.cs
@@ -28,7 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.AllowDrop = true;
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(803, 452);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            this.tabControl1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.tabControl1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.AllowDrop = true;
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(795, 426);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "出現ボス";
+            this.tabPage1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.tabPage1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.tabPage1.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage1_Paint);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.AllowDrop = true;
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(795, 426);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "凸推移";
+            this.tabPage2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.tabPage2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.tabPage2.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage2_Paint);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.AllowDrop = true;
+            this.tabPage3.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(795, 426);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "凸速度";
+            this.tabPage3.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.tabPage3.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.tabPage3.Paint += new System.Windows.Forms.PaintEventHandler(this.tabPage3_Paint);
             // 
             // Form1
             // 
@@ -36,16 +101,23 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
     }
 }
 
--- CranBattleDefeatLogViewer/Form1.cs
+++ CranBattleDefeatLogViewer/Form1.cs
@@ -19,27 +19,34 @@ namespace CranBattleDefeatLogViewer
             InitializeComponent();
         }
 
-        List<DateTime> datelist = null;
+        List<DateTime> defeatlist = null;
+        List<DateTime> attacklist = null;
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            var g = e.Graphics;
-
-
-            if (datelist != null)
-            {
-                DrawGraph(g, datelist);
-            }
-            else
-            {
-                Font fnt = new Font("MS UI Gothic", 12);
-
-                g.DrawString("行動ファイルをドラッグ＆ドロップしてください", fnt, Brushes.Black, 0, 0);
-            }
-
         }
 
-        private void DrawGraph(Graphics g, List<DateTime> datelist)
+        IEnumerable<DateTime> LastAdd(List<DateTime> datelist)
+        {
+            foreach (var item in datelist)
+            {
+                yield return item;
+            }
+            if (0 < datelist.Count)
+            {
+                var last = datelist.Last();
+                if (last.Hour < 5)
+                {
+                    yield return new DateTime(last.Year, last.Month, last.Day, 4, 59, 59);
+                }
+                else
+                {
+                    yield return new DateTime(last.Year, last.Month, last.Day, 23, 59, 59);
+                }
+            }
+        }
+
+        private void DrawBossGraph(Graphics g, List<DateTime> datelist)
         {
             Brush[] brush =
             {
@@ -80,7 +87,7 @@ namespace CranBattleDefeatLogViewer
 
             y += 16;
             g.DrawString(starttime.Day.ToString(), fnt, Brushes.Black, 4, y + 8);
-            foreach (var dt in datelist)
+            foreach (var dt in LastAdd(datelist))
             {
                 int bindex = boss % 5;
 
@@ -104,6 +111,237 @@ namespace CranBattleDefeatLogViewer
                 prevtime = dt;
 
                 boss++;
+            }
+
+        }
+
+        IEnumerable<List<Point>> DayData(List<DateTime> datelist)
+        {
+            var start = datelist[0];
+            var prevtime = new DateTime(start.Year, start.Month, start.Day, 5, 0, 0);
+            var limittime = prevtime.AddDays(1);
+            var starttime = prevtime;
+
+            int attacknum = 0;
+            int day = 0;
+
+            List<Point> result = new List<Point>();
+            result.Add(new Point(0, 0));
+
+            foreach (var dt in datelist)
+            {
+                if (limittime <= dt)
+                {
+                    result.Add(new Point( Px(starttime.AddDays(1), starttime), attacknum - 1));
+                    yield return result;
+
+                    result.Clear();
+                    prevtime = limittime;
+                    limittime = limittime.AddDays(1);
+                    starttime = starttime.AddDays(1);
+                    attacknum = 0;
+                    day++;
+
+                    result.Add(new Point(0, 0));
+                }
+
+                result.Add(new Point(Px(dt, starttime), attacknum));
+
+                prevtime = dt;
+                attacknum++;
+            }
+            yield return result;
+        }
+
+
+
+        IEnumerable<List<PointF>> AttackSpeedList(List<DateTime> datelist)
+        {
+            foreach (var daylist in DayData(datelist))
+            {
+                var list = new List<PointF>();
+
+                int left = 0;
+                int right = 0;
+
+                var lastx = daylist.Last().X;
+                for(int i = 0; i < lastx; i++)
+                {
+                    float lvalue = CalcValue(daylist, ref left, i - 15);
+                    float rvalue = CalcValue(daylist, ref right, i + 15);
+
+                    list.Add(new PointF(i, rvalue - lvalue));
+                }
+
+                yield return list;
+            }
+        }
+
+        private static float CalcValue(List<Point> daylist, ref int index, int xvalue)
+        {
+            for (; index < daylist.Count; index++)
+            {
+                if (xvalue < daylist[index].X) break;
+            }
+            if (daylist.Count <= index)
+            {
+                return (float) daylist[daylist.Count - 1].Y;
+            }
+
+            if (0 < index)
+            {
+                var prev = daylist[index - 1];
+                var now = daylist[index];
+                float rate = (xvalue * 1.0f - prev.X) / (now.X - prev.X);
+                return prev.Y * (1 - rate) + now.Y * rate;
+            }
+            else
+            {
+                return (float)daylist[0].Y;
+            }
+
+        }
+
+        private void DrawAttackGraph(Graphics g, List<DateTime> datelist)
+        {
+            Brush[] brush =
+            {
+                Brushes.CornflowerBlue,
+                Brushes.Aquamarine,
+                Brushes.ForestGreen,
+                Brushes.Gold,
+                Brushes.OrangeRed,
+                Brushes.Red,
+            };
+            Color[] colors =
+            {
+                Color.CornflowerBlue,
+                Color.Aquamarine,
+                Color.ForestGreen,
+                Color.Gold,
+                Color.OrangeRed,
+                Color.Red,
+            };
+
+            var ox = 32;
+            var y = 48;
+            var start = datelist[0];
+            var prevtime = new DateTime(start.Year, start.Month, start.Day, 5, 0, 0);
+            var limittime = prevtime.AddDays(1);
+            var starttime = prevtime;
+            Font fnt = new Font("MS UI Gothic", 12);
+
+            const int attackheight = 3;
+
+            var dispday = start;
+            for (int i = 0; i < 5; i++)
+            {
+                g.FillRectangle(brush[i], i * 48 + 32, 8, 48, 16);
+                g.DrawString($"{dispday.Month}/{dispday.Day}", fnt, Brushes.Black, i * 48 + 40, 8);
+                dispday = dispday.AddDays(1);
+            }
+
+
+            for (int i = 0; i < 25; i++)
+            {
+                int x = ox + i * 30;
+                g.DrawLine(Pens.White, x, 32, x, 32 + 300);
+
+                g.DrawString(((i + 5) % 24).ToString(), fnt, Brushes.Black, x, 32);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int ly = y + i * attackheight * 10 + 16;
+                g.DrawLine(Pens.White, 0, ly, 25 * 30+ 32, ly);
+
+                g.DrawString(((9 - i) * 10).ToString(), fnt, Brushes.Black, 4, ly - 16);
+
+            }
+
+            var day = 0;
+            foreach (var pointrawlist in DayData(datelist))
+            {
+                using (var pen = new Pen(colors[day], 2))
+                {
+                    var drawline = pointrawlist.Select(n => new Point(ox + n.X, y + 16 + (90 - n.Y) * attackheight)).ToArray();
+                    g.DrawLines(pen, drawline);
+
+//                    var liney = y + (90 - attacknum) * attackheight + 16;
+//                    g.DrawLine(pen[day], ox + Px(prevtime, starttime), liney, ox + Px(dt, starttime), liney - attackheight);
+                }
+
+                day++;
+            }
+        }
+
+        private void DrawAttackSpeedGraph(Graphics g, List<DateTime> datelist)
+        {
+            Brush[] brush =
+            {
+                Brushes.CornflowerBlue,
+                Brushes.Aquamarine,
+                Brushes.ForestGreen,
+                Brushes.Gold,
+                Brushes.OrangeRed,
+                Brushes.Red,
+            };
+            Color[] colors =
+            {
+                Color.CornflowerBlue,
+                Color.Aquamarine,
+                Color.ForestGreen,
+                Color.Gold,
+                Color.OrangeRed,
+                Color.Red,
+            };
+
+            var ox = 32;
+            var y = 48;
+            var start = datelist[0];
+            var prevtime = new DateTime(start.Year, start.Month, start.Day, 5, 0, 0);
+            var limittime = prevtime.AddDays(1);
+            var starttime = prevtime;
+            Font fnt = new Font("MS UI Gothic", 12);
+
+            const int attackheight = 30;
+
+            var dispday = start;
+            for (int i = 0; i < 5; i++)
+            {
+                g.FillRectangle(brush[i], i * 48 + 32, 8, 48, 16);
+                g.DrawString($"{dispday.Month}/{dispday.Day}", fnt, Brushes.Black, i * 48 + 40, 8);
+                dispday = dispday.AddDays(1);
+            }
+
+
+            for (int i = 0; i < 25; i++)
+            {
+                int x = ox + i * 30;
+                g.DrawLine(Pens.White, x, 32, x, 32 + 300);
+
+                g.DrawString(((i + 5) % 24).ToString(), fnt, Brushes.Black, x, 32);
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int ly = y + i * attackheight + 16;
+                g.DrawLine(Pens.White, 0, ly, 25 * 30 + 32, ly);
+
+                g.DrawString(((9 - i)).ToString(), fnt, Brushes.Black, 4, ly - 16);
+
+            }
+
+            var day = 0;
+            foreach (var pointrawlist in AttackSpeedList(datelist))
+            {
+                using (var pen = new Pen(colors[day], 2))
+                {
+                    var drawline = pointrawlist.Select(n => new Point((int) (ox + n.X), (int)( y + 16 + (9 - n.Y) * attackheight))).ToArray();
+                    g.DrawLines(pen, drawline);
+                }
+
+                day++;
             }
         }
 
@@ -130,32 +368,44 @@ namespace CranBattleDefeatLogViewer
 
             if (0 < fileName.Length)
             {
-                List<DateTime> d = new List<DateTime>();
-                using (var sm = new StreamReader(fileName[0]))
+                switch (tabControl1.SelectedIndex)
                 {
-                    while (0 <= sm.Peek())
-                    {
-                        var line = sm.ReadLine();
-                        try
-                        {
-                            var date = DateTime.Parse(line);
-                            d.Add(date);
-                        }
-                        catch (FormatException)
-                        {
+                    case 0:
+                        defeatlist = LoadDateFile(fileName[0]);
+                        break;
+                    case 1:
+                    case 2:
+                        attacklist = LoadDateFile(fileName[0]);
+                        break;
+                    default:
+                        break;
+                }
 
-                        }
+                this.Refresh();
+            }
+        }
+
+        private List<DateTime> LoadDateFile(string fileName)
+        {
+            List<DateTime> d = new List<DateTime>();
+            using (var sm = new StreamReader(fileName))
+            {
+                while (0 <= sm.Peek())
+                {
+                    var line = sm.ReadLine();
+                    try
+                    {
+                        var date = DateTime.Parse(line);
+                        d.Add(date);
+                    }
+                    catch (FormatException)
+                    {
+
                     }
                 }
-
-                if (0 < d.Count)
-                {
-                    datelist = d;
-                    var last = datelist.Last();
-                    datelist.Add(new DateTime(last.Year, last.Month, last.Day, last.Hour < 5 ? 4 : 23, 59, 0));
-                    this.Refresh();
-                }
             }
+
+            return d;
         }
 
         private void Form1_DragEnter(object sender, DragEventArgs e)
@@ -167,6 +417,55 @@ namespace CranBattleDefeatLogViewer
             else
                 //ファイル以外は受け付けない
                 e.Effect = DragDropEffects.None;
+        }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void tabPage1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            if (defeatlist != null)
+            {
+                DrawBossGraph(g, defeatlist);
+            }
+            else
+            {
+                Font fnt = new Font("MS UI Gothic", 12);
+                g.DrawString("defeatlogをドラッグ＆ドロップしてください", fnt, Brushes.Black, 0, 0);
+            }
+        }
+
+        private void tabPage2_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            if (attacklist != null)
+            {
+                DrawAttackGraph(g, attacklist);
+            }
+            else
+            {
+                Font fnt = new Font("MS UI Gothic", 12);
+                g.DrawString("attacklogをドラッグ＆ドロップしてください", fnt, Brushes.Black, 0, 0);
+            }
+        }
+
+        private void tabPage3_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+            if (attacklist != null)
+            {
+                DrawAttackSpeedGraph(g, attacklist);
+            }
+            else
+            {
+                Font fnt = new Font("MS UI Gothic", 12);
+                g.DrawString("attacklogをドラッグ＆ドロップしてください", fnt, Brushes.Black, 0, 0);
+            }
+
         }
     }
 }
